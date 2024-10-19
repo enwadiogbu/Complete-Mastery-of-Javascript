dff64c5747e288a6bfd805e31a343a2e84a5409f
enwadiogbu <emekanwadiogbu.jr@me.com>
2024-10-18 18:58:37 -0700

    relearning booleans in javascript

--- script.js
+++ script.js
@@ -13,12 +13,20 @@ const d = b + c;
 console.log(d);
 
 
-// Backticks as strings
+// Backticks as strings are used to create a dynamic string of characters
 const age = 45;
 console.log(`Emeka is ${age}`);
 
+// Booleans - represent a logical entity and can only have two possible values (true or false)
 
+const didVote = 80;
+const e = `Did 65% of the population in Texas vote? ${didVote}`;
 
+if(didVote >= 65) {
+    console.log(e, true);
+}else {
+    console.log(e, false);
+}
 
 
 
